2f37b5efd1b09bcccd0cd201612f0cf919fd9685
Wang Xiaobo <draculaw@live.com>
2015-05-05 13:29:47 +0800

    update csv use ; instead of ,

--- update.py
+++ update.py
@@ -9,11 +9,19 @@ if __name__ == "__main__":
     c = db.cursor()
     cmd = "select distinct code from stock"
     c.execute(cmd)
-    list = c.fetchall()
+    ll = c.fetchall()
     c.close()
+    if  isinstance(ll[0], tuple):
+        
+        ll = [d[0] for d in ll]
+    elif isinstance(ll[0], str):
+        
+        pass
+    else:
+        raise "The database has some issue"
 
     start = datetime.datetime.now()
     print "Start:", start
-    get.update_list(list)
+    get.update_list(ll)
     print "Start %s End: %s" %(start.strftime("%Y-%m-%d %H:%M:%S:%Z"), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S:%Z"))
     
